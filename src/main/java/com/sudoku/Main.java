c735a249b2b2f98ac560a47e7691d560118c9324
Camilo Portilla Ortega <portilla.camilo@correounivalle.edu.co>
2025-04-10 22:37:28 -0500

    Menú de inicio, botones Jugar, ¿Cómo jugar?, Creditos y Salir, añadidos

--- src/main/java/com/sudoku/Main.java
+++ src/main/java/com/sudoku/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/sudoku/vista/game-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/com/sudoku/vista/menu-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 574, 574);
         stage.setTitle("Sudoku!");
         stage.setScene(scene);
